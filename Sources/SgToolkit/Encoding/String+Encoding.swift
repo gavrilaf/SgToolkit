b88eb80b891729eeb66a5a3de57cd1e57cda0903
eugenf <eugenf78@gmail.com>
2018-06-20 23:12:59 +0300

    fixing Base32, added tests

--- Sources/SgToolkit/Encoding/String+Encoding.swift
+++ Sources/SgToolkit/Encoding/String+Encoding.swift
@@ -18,6 +18,7 @@ extension String {
             return Base32.encode(pointer: $0.baseAddress!, length: $0.count - 1)
         }
     }
+    
     public func base32DecodedString(encoding: String.Encoding = .utf8) -> String? {
         return Base32.decode(string: self).flatMap {
             return String(data: $0, encoding: encoding)
--- Tests/SgToolkitTests/Base32Tests.swift
+++ Tests/SgToolkitTests/Base32Tests.swift
@@ -10,14 +10,26 @@ import XCTest
 
 class Base32Tests: XCTestCase {
 
-    let samples = [("", ""),
-                   ("f", "MY======"),
-                   ("fo", "MZXQ===="),
-                   ("foo", "MZXW6==="),
-                   ("foob", "MZXW6YQ="),
-                   ("fooba", "MZXW6YTB"),
-                   ("foobar", "MZXW6YTBOI======"),
-                   ]
+    let samples = [
+        // RFC 4648 examples
+        ("", ""),
+        ("f", "MY======"),
+        ("fo", "MZXQ===="),
+        ("foo", "MZXW6==="),
+        ("foob", "MZXW6YQ="),
+        ("fooba", "MZXW6YTB"),
+        ("foobar", "MZXW6YTBOI======"),
+        
+        // Wikipedia examples
+        ("sure.", "ON2XEZJO"),
+        ("sure", "ON2XEZI="),
+        ("sur", "ON2XE==="),
+        ("su", "ON2Q===="),
+        ("leasure.", "NRSWC43VOJSS4==="),
+        ("easure.", "MVQXG5LSMUXA===="),
+        ("asure.", "MFZXK4TFFY======"),
+        ("sure.", "ON2XEZJO"),
+    ]
     
     
     func testBase32Decoding() {
@@ -32,28 +44,10 @@ class Base32Tests: XCTestCase {
         }
     }
     
-    func testAllValues() {
-        // Generated with:
-        // python -c 'import base64; print(base64.b32encode("".join([chr(x) for x in range(0, 256)]).encode()))'
-        let allValues = """
-                        AAAQEAYEAUDAOCAJBIFQYDIOB4IBCEQTCQKRMFYYDENBWHA5DYPSAIJCEMSCKJRHFAUSUKZMFUXC6MBRGIZTINJWG44DSOR
-                        3HQ6T4P2AIFBEGRCFIZDUQSKKJNGE2TSPKBIVEU2UKVLFOWCZLJNVYXK6L5QGCYTDMRSWMZ3INFVGW3DNNZXXA4LSON2HK5T
-                        XPB4XU634PV7H7QUAYKA4FAWCQPBIJQUFYKDMFB6CRDBITQUKYKF4FDGCRXBI5QUPYKIMFEOCSLBJHQUUYKK4FFWCS7BJRQU
-                        ZYKNMFG6CTTBJ3QU6YKP4FIGCUHBKFQVDYKSMFJOCU3BKPQVIYKU4FKWCVPBKZQVNYKXMFL6CWDBLDQVSYKZ4FNGCWXBLNQV
-                        XYK4MFOOCXLBLXQV4YK64FPWCX7BYBQ4BYOBMHA6DQTBYLQ4GYOD4HCGDRHBYVQ4LYOGMHDODR3BY7Q4QYOI4HEWDSPBZJQ4
-                        VYOLMHF6DTDBZTQ42YON4HHGDTXBZ5Q47YOQMHIODULB2HQ5EYOS4HJWDU7B2RQ5JYOVMHK6DVTB23Q5OYOX4HMGDWHB3FQ5
-                        TYO2MHNODW3B3PQ5YYO44HOWDXPB3ZQ55YO7MHPY=
-                        """
-        
-        let data = Base32.decode(string: allValues)
-        let encoded = Base32.encode(data: data ?? Data())
-        
-        XCTAssertEqual(allValues, encoded)
-    }
+    
     
     static var allTests = [
         ("testBase32Decoding", testBase32Decoding),
         ("testBase32Encoding", testBase32Encoding),
-        ("testAllValues", testAllValues),
     ]
 }
